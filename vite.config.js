84ccd442055ec7b5a77849d93daaac67ddbc0071
fiskhandlarn <oskar@joelson.org>
2022-05-26 22:30:41 +0200

    Build favicons and include them, remove vite hot reload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import viteStylelint from '@amatlash/vite-plugin-stylelint';
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 
 require('dotenv').config();
 
@@ -7,22 +8,42 @@ export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : '/build/',
   publicDir: 'resources/assets/static',
   build: {
-    manifest: true,
+    manifest: '_manifest.json',
     outDir: `public/themes/${process.env.WP_DEFAULT_THEME}/assets`,
     assetsDir: '',
     rollupOptions: {
       input: 'resources/assets/scripts/index.js',
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.css$/.test(assetInfo.name)) {
+            return '[name].[hash][extname]';
+          }
+          return '[name][extname]';
+        },
+        manualChunks: null,
+      },
     },
   },
   plugins: [
     viteStylelint(),
-    {
-      name: 'php',
-      handleHotUpdate({ file, server }) {
-        if (file.endsWith('.php')) {
-          server.ws.send({ type: 'full-reload', path: '*' });
-        }
+    ViteFaviconsPlugin({
+      logo: 'resources/assets/static/images/favicons/favicon.png',
+      favicons: {
+        appName: 'DoomtrooperDB',
+        background: '#000',
+        theme_color: '#fff',
+        path: "/themes/" + process.env.WP_DEFAULT_THEME + "/assets/images/favicons/",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: false,
+          coast: true,
+          favicons: true,
+          firefox: false,
+          windows: true,
+          yandex: true,
+        },
       },
-    },
+    }),
   ],
 }));
